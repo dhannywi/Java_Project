90717f567e01b6f8589504f789904e8b658640af
Dhanny Indrakusuma <dwindrakusuma@gmail.com>
2025-05-06 22:38:56 -0500

    hello updated

--- hello/src/MyFirstProgram.java
+++ hello/src/MyFirstProgram.java
@@ -1,5 +1,8 @@
 public class MyFirstProgram {
     public static void main(String[] args){
         System.out.println("Hello, world");
+        System.out.println("I'm a Programmer");
+        System.out.println("Let’s change the code now…");
+        System.out.println("it’s what I do!");
     }
 }
