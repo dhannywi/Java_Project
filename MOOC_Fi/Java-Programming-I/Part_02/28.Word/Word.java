46f8839c452218576781e85330afe344ee6c627f
Dhanny Indrakusuma <dwindrakusuma@gmail.com>
2025-06-13 17:03:28 -0500

    add code for Part02-27 & 28

---
+++ MOOC_Fi/Java-Programming-I/Part_02/28.Word/Word.java
@@ -0,0 +1,13 @@
+// Write a method public static String word(). The method must return a string of your choice.
+import java.util.Scanner;
+
+public class Word {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        word();
+    }
+    public static String word() {
+        return "Hello there! How are you?";
+    }
+}
